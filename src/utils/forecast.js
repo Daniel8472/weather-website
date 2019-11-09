ca7c8447cb4a061f40c17fd4b772e63337b749f8
daniel8472 <danielbamford@live.com>
2019-11-09 11:28:02 +0000

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (a, b, callback) => {
+    const url = 'https://api.darksky.net/forecast/19727542a0ae299c55cc6d0b7e5c70e2/' + encodeURIComponent(a)+ ',' + encodeURIComponent(b) +'?units=si'
+
+    request({ url, json: true }, (error, { body } ) => {
+        if (error) {
+            callback('Unable to connect to weather services.')
+        } else if (body.error) {
+            callback('Unable to find location provided.')
+        } else {
+            callback(undefined, {
+                temp: body.currently.temperature,
+                rain: body.currently.precipProbability,
+                summary: body.daily.data[0].summary
+            })
+        }
+    })
+}
+
+module.exports = forecast
